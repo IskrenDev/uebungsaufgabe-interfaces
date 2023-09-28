921b73d01436749658996b707290ebcf1812ff48
iskrenr <iskrenr005@gmail.com>
2023-09-28 11:29:47 +0200

    Create instance of class 'MediaController' and objects of the 'MusicPlayer' und 'VideoPlayer' classes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,11 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        MediaController mediaController1 = new MediaController();
+        VideoPlayer videoPlayer1 = new VideoPlayer();
+        MusicPlayer musicPlayer1 = new MusicPlayer();
+
+        mediaController1.playMedia(videoPlayer1);
+        mediaController1.playMedia(musicPlayer1);
     }
 }
